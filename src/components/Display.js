c9e3da0faa3fcbfd2a69fea9802f7ddf371c164d
chinch42 <vanessakolund@gmail.com>
2022-06-29 13:57:02 +0200

    axios error handler

---
+++ src/components/Display.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Display = () => {
+  return (
+    <div id="ul">
+      <h3>Topic 2</h3>
+      <ul>
+        <li>username</li>
+        <li>2 hours ago</li>
+        <li>comments</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Display;
--- src/components/GetData.js
+++ src/components/GetData.js
@@ -1,13 +1,51 @@
 import React, { useEffect } from "react";
+import axios from "axios";
+//using GetDat = async (inputform) => ?
+const GetData = ({ id }) => {
+  const axios = require("axios");
+  // await axios.get(url, { params: { answer: 42 } });
+  // url?params=answer, example: ...search?query=story
+  //no await possible, even i took async fetchdata
+  useEffect(() => {
+    const fetchdata = (id) => {
+      if (!id) {
+        let story;
+        id = story;
+      }
+      try {
+        const res = axios({
+          url: "http://hn.algolia.com/api/v1/search",
+          method: "get",
 
-const GetData = ({ inputform, URL }) => {
-  const stringurl = URL + inputform;
+          params: { query: id },
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        res
+          .then((response) => response.data)
+          .then((data) => {
+            console.log(data.hits);
+          });
+
+        // console.log("Try");
+      } catch (err) {
+        if (err.response) {
+          // The client was given an error response (5xx, 4xx)
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+          console.log("Error: " + err.message);
+        }
+      }
+    };
+    fetchdata(id);
+  }, []);
 
   return (
     <div>
-      <ul id="ul">
-        <li>console.log({stringurl});</li>
-      </ul>
+      <h1></h1>
     </div>
   );
 };
