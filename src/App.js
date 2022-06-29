28479a0c97fd0fdf2b062004b3bef624f2a31a20
chinch42 <vanessakolund@gmail.com>
2022-06-29 08:43:14 +0200

    update header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Test from "./components/Test";
+import Header from "./components/Header";
 
 const API_URL = "http://hn.algolia.com/api/v1/search?query=";
 
@@ -16,50 +17,12 @@ function App() {
   let stringselection = "story";
   return (
     <div>
-      <header>
-        <nav>
-          <ul className="menu">
-            <li>
-              <a id="a" href="#">
-                Home
-              </a>
-            </li>
-            <li>
-              <a id="a" href="#">
-                Stories
-              </a>
-            </li>
-            <li>
-              <a id="a" href="#">
-                News
-              </a>
-            </li>
-            <li>
-              <a id="a" href="#">
-                Contact
-              </a>
-            </li>
-            <li>
-              <form>
-                {/* <UserContext.Provider value={inputform.value}> */}
-                <input
-                  id="input"
-                  type="text"
-                  placeholder="Search..."
-                  value={inputform}
-                  onChange={handleChange}
-                />
-                {/* </UserContext.Provider> */}
-              </form>
-            </li>
-            <li>
-              <button id="inputbutton" onClick={handleClick} type="submit">
-                <i className="fa fa-plus  inpuntbutton">Search</i>
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Header
+        handleChange={handleChange}
+        inputform={inputform}
+        hanhdleClick={handleClick}
+        setInputform={setInputform}
+      />
       <Test inputform={inputform} />
       <article></article>
     </div>
